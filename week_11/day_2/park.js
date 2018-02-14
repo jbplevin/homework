f97e43fd5d7ae7730b5f98f01a9d9bbb9018a4b9
User <joe.plevin@hotmail.co.uk>
2018-02-14 10:11:41 +0000

    park remove by type test added and passed

--- week_11/day_2/park.js
+++ week_11/day_2/park.js
@@ -4,4 +4,12 @@ const Park = function(){
 Park.prototype.addDinosaur = function(dinosaur){
   this.enclosure.push(dinosaur);
 }
+Park.prototype.removeByType = function(type){
+  var enclosure = this.enclosure;
+  for(dinosaur of enclosure){
+    if(dinosaur.type === type){
+      enclosure.splice(enclosure.indexOf(dinosaur), 1)
+    }
+  }
+}
 module.exports = Park;
--- week_11/day_2/specs/park_spec.js
+++ week_11/day_2/specs/park_spec.js
@@ -8,13 +8,17 @@ describe('Park', function(){
 
   beforeEach(function(){
     park = new Park();
-    dinosaur = new Dinosaur('Velociraptor', 2);
+    dinosaur1 = new Dinosaur('Velociraptor', 2);
+    park.addDinosaur(dinosaur1);
   })
   it('should have an enslosure', function(){
-    assert.deepStrictEqual(park.enclosure, []);
+    assert.deepStrictEqual(  park.enclosure, [dinosaur1]);
   })
   it('should be able to add dinosaurs', function(){
-    park.addDinosaur(dinosaur);
     assert.strictEqual(park.enclosure.length, 1);
+  })
+  it('should be able to remove dinosaurs by type', function(){
+    park.removeByType('Velociraptor');
+    assert.strictEqual(park.enclosure.includes('Velociraptor'), false);
   })
 })
