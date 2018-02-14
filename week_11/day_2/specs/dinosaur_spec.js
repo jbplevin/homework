bc1a20355f3772eb2940cb87dca127ea4cd3ae08
User <joe.plevin@hotmail.co.uk>
2018-02-14 08:41:09 +0000

    dinosaur offspring test added and passed

--- week_11/day_2/specs/dinosaur_spec.js
+++ week_11/day_2/specs/dinosaur_spec.js
@@ -6,11 +6,15 @@ describe('Dinosaur', function(){
   let dinosaur;
 
   beforeEach(function(){
-    dinosaur = new Dinosaur('Velociraptor');
+    dinosaur = new Dinosaur('Velociraptor', 2);
   })
 
   it('should have a type', function(){
     const actual = dinosaur.type;
     assert.strictEqual(actual, 'Velociraptor');
+  })
+  it('should have a number of offspring per year', function(){
+    const actual = dinosaur.offspringPerYear;
+    assert.strictEqual(actual, 2);
   })
 })
